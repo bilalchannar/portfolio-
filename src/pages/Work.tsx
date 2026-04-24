d8d6e6fa6104ee84ddbc93764509161e23caeab8
GitTower <bilalchannar@users.noreply.github.com>
2026-04-24 17:18:15 +0500

    Update Work section to use native horizontal scroll and remove internal vertical scroll

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -1,53 +1,8 @@
 import "../components/styles/Work.css";
 import WorkImage from "../components/common/WorkImage";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { useEffect } from "react";
 import { config } from "../config";
 
-gsap.registerPlugin(ScrollTrigger);
-
 const Work = () => {
-  useEffect(() => {
-    let translateX: number = 0;
-
-    function setTranslateX() {
-      const box = document.getElementsByClassName("work-box");
-      if (box.length === 0) return;
-      const rectLeft = document
-        .querySelector(".work-container")!
-        .getBoundingClientRect().left;
-      const rect = box[0].getBoundingClientRect();
-      const parentWidth = box[0].parentElement!.getBoundingClientRect().width;
-      let padding: number =
-        parseInt(window.getComputedStyle(box[0]).padding) / 2;
-      translateX = rect.width * box.length - (rectLeft + parentWidth) + padding;
-    }
-
-    setTranslateX();
-
-    let timeline = gsap.timeline({
-      scrollTrigger: {
-        trigger: ".work-section",
-        start: "center bottom",
-        end: `+=${translateX}`, // Use actual scroll width
-        scrub: true,
-        pin: false,
-        id: "work",
-      },
-    });
-
-    timeline.to(".work-flex", {
-      x: -translateX,
-      ease: "none",
-    });
-
-    // Clean up
-    return () => {
-      timeline.kill();
-      ScrollTrigger.getById("work")?.kill();
-    };
-  }, []);
   return (
     <div className="work-section" id="work">
       <div className="work-container section-container">
